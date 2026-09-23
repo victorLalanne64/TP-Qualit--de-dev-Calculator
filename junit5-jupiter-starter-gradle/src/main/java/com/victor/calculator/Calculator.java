548fdd4459a605b2ee01503c4353199bb06d478a
Victor Lalanne <vlalanne001@iutbayonne.univ-pau.fr>
2026-09-23 12:00:15 +0200

    Question 6 du tp et correction de bug

--- junit5-jupiter-starter-gradle/src/main/java/com/victor/calculator/Calculator.java
+++ junit5-jupiter-starter-gradle/src/main/java/com/victor/calculator/Calculator.java
@@ -1,4 +1,16 @@
 package com.victor.calculator;
 
 public class Calculator {
+
+    public static int add(int opG, int opD) {
+        long result = (long) opG + (long) opD;
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+            throw new ArithmeticException("Dépassement de capacité pour un int");
+        }
+        return (int) result;
+    }
+
+    public static int divide(int opG, int opD) {
+        return opG / opD;
+    }
 }
--- junit5-jupiter-starter-gradle/src/test/java/com/victor/calculator/CalculatorTest.java
+++ junit5-jupiter-starter-gradle/src/test/java/com/victor/calculator/CalculatorTest.java
@@ -1,7 +1,34 @@
 package com.victor.calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CalculatorTest {
 
+    @Test
+    void add_devrait_calculer_la_somme_de_deux_int() {
+        // GIVEN
+        int a = 2;
+        int b = 3;
+
+        // WHEN
+        int resultat = Calculator.add(a, b);
+
+        // THEN
+        assertThat(resultat).isEqualTo(5);
+    }
+
+    @Test
+    void divide_devrait_calculer_le_quotient_de_deux_int() {
+        // GIVEN
+        int opG = 10;
+        int opD = 2;
+
+        // WHEN
+        int resultat = Calculator.divide(opG, opD);
+
+        // THEN
+        assertThat(resultat).isEqualTo(5);
+    }
 }
